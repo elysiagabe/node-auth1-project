d95bafb90f82e4468ed0eaa0ba836b3c948a1321
Elysia Gabe <elysiagabe@gmail.com>
2020-04-20 14:48:32 -0700

    Creates react app

--- users/user-router.js
+++ users/user-router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Users = require('../users/user-model');
 
 // ~~ ENDPOINTS ~~ //
-// SEE ALL USERS -- GET all users, restricted to logged in users only
+// SEE ALL USERS -- GET all users, restricted to logged in users only (authenticator middleware applied in server.js)
 router.get('/', (req, res) => {
     Users.find()
         .then(users => {
@@ -10,6 +10,21 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ errorMessage: "Could not find list of users" })
+        })
+})
+
+// SPECIFIC USER -- GET user by id, restricted to logged in users only (authenticator middleware applied in server.js)
+router.get('/:id', (req, res) => {
+    Users.findById(req.params.id)
+        .then(user => {
+            if(user) {
+                res.json(user);
+            } else {
+                res.status(401).json({ errorMessage: "There is no user with the specified id" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "Could not get specified user" })
         })
 })
 
